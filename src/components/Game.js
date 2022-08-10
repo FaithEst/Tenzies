c2c515d0e04e7a0063afa3c887da24fbdcd05b2f
Faith Mutitu <mutitufaith53@gmail.com>
2022-08-10 15:36:43 +0300

    updated challenge

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import './game.css';
 import Die from "./Die";
 
+
+/**
+ * Challenge:
+ * 
+ * Create state to hold our array of numbers. (Initialize
+ * the state by calling our `allNewDice` function so it 
+ * loads all new dice as soon as the app loads)
+ * 
+ * Map over the state numbers array to generate our array
+ * of Die elements and render those in place of our
+ * manually-written 10 Die elements.
+ */
 export default function Game(){
+    const [numbersArray, setNumbersArray] = useState(allNewDice());
+
+    function allNewDice(){
+        const numbers = [];
+        for(let num = 0; num < 10; num++){
+            numbers.push(Math.ceil(Math.random() * 6));
+        }
+        return numbers;
+    }
+    
+    const die = numbersArray.map(num => <Die value={num}/>)
+
     return(
         <div className="game">
             <h1>Tenzies</h1>
@@ -12,7 +36,7 @@ export default function Game(){
                 its current value between rolls.
             </p>
             <div className="dice--one">
-                <Die value="1"/>
+                {die}
             </div>
         </div>
     )
