622e77d284a87a8108ff2bd3c21c18710dc2e57d
Faith Mutitu <mutitufaith53@gmail.com>
2022-08-12 19:57:24 +0300

    adds conditional styling

--- src/components/Die.js
+++ src/components/Die.js
@@ -2,7 +2,10 @@ import React from "react";
 import './game.css';
 
 export default function Die(props){
+    const styles = {
+        backgroundColor: props.isHeld ? "green" : "white"
+    }
     return(
-        <div className="dice">{props.value}</div>
+        <div className="dice" style={styles}>{props.value}</div>
     )
 }
