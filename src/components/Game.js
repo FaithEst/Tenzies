be7f7fb6542adfa0304de3ed5fffcbe206e31ae6
Faith Mutitu <mutitufaith53@gmail.com>
2022-08-12 19:57:58 +0300

    adds property to Die component

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import './game.css';
 import Die from "./Die";
-
+import { nanoid } from "nanoid";
 
 /**
  * Challenge:
@@ -23,20 +23,49 @@ import Die from "./Die";
  * and set the `dice` state to that new array (thus re-rendering
  * the array to the page)
  */
-export default function Game(){
-    const [numbersArray, setNumbersArray] = useState(allNewDice());
 
-    function allNewDice(){
+/**
+ * Challenge: Update the array of numbers in state to be
+ * an array of objects instead. Each object should look like:
+ * { value: <random number>, isHeld: false }
+ * 
+ * Making this change will break parts of our code, so make
+ * sure to update things so we're back to a working state
+ */
+
+/**
+ * Challenge: Add conditional styling to the Die component
+ * so that if it's held (isHeld === true), its background color
+ * changes to a light green (#59E391)
+ * 
+ * Remember: currently the Die component has no way of knowing
+ * if it's "held" or not.
+ */
+
+export default function Game(){
+    const [diceObjectArray, setdiceObjectArray] = useState(allNewDice());
+
+    function allNewDice(){    //returns an object with array of 10 random numbers from 1-6 (inclusive)
         const numbers = [];
         for(let num = 0; num < 10; num++){
-            numbers.push(Math.ceil(Math.random() * 6));
+            numbers.push({
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false,
+                id: nanoid()
+            });
         }
         return numbers;
     }
-    function rollDice(){
-        setNumbersArray(allNewDice())
+    function rollDice(){   //regenerates another set of random numbers
+        setdiceObjectArray(allNewDice())
     }
-    const die = numbersArray.map(num => <Die value={num}/>)
+    const die = diceObjectArray.map(num => 
+        <Die 
+            key={num.id} 
+            value={num.value}
+            isHeld={num.isHeld}
+        />
+    )
     console.log("component render")
     return(
         <div className="game">
