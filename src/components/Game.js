cdc111caf1790728870320d7505f94dc744adfbf
Faith Mutitu <mutitufaith53@gmail.com>
2022-08-13 21:45:31 +0300

    adds toggle functionality on isHeld prop

--- src/components/Game.js
+++ src/components/Game.js
@@ -54,6 +54,18 @@ import { nanoid } from "nanoid";
  * you want)
  * 
  */
+
+/**
+ * Challenge: Update the `holdDice` function to flip
+ * the `isHeld` property on the object in the array
+ * that was clicked, based on the `id` prop passed
+ * into the function.
+ * 
+ * Hint: as usual, there's > 1 way to accomplish this.
+ * I'll be using `dice.map()` and checking for the `id`
+ * of the die to determine which one to flip `isHeld` on,
+ * but you can do whichever way makes the most sense to you.
+ */
 export default function Game(){
     const [diceObjectArray, setdiceObjectArray] = useState(allNewDice());
 
@@ -72,7 +84,11 @@ export default function Game(){
         setdiceObjectArray(allNewDice())
     }
     function holdDice(id){
-        console.log(id)
+        setdiceObjectArray(prevArray => prevArray.map(dice => (
+            id === dice.id ?
+            {...dice, isHeld: !dice.isHeld} : 
+            dice
+        )))
     }
     const die = diceObjectArray.map(num => 
         <Die 
